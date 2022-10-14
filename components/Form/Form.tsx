be06f0b9863b7150d798b41185bb622d0eba4f16
nathaliariascos <riascosnathalia6@gmail.com>
2022-10-14 15:00:19 -0500

    chore: add emailjs

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,8 +1,18 @@
+import { useState, MouseEvent, ChangeEvent } from 'react'
 import styles from './Form.module.css'
+import emailjs from '@emailjs/browser'
+
 
 const Form = () => {
+
+  const handleSubmit = (e: MouseEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    emailjs.sendForm('service_9pe6xje', 'template_98ujsqb', e.target, 'xAHlbvaWXBdQmMKEZ')
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+  }
   return (
-    <form id={styles.form}>
+    <form id={styles.form} onSubmit={handleSubmit}>
       <input 
         className={styles.input}
         type='text'
